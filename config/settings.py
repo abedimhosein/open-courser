981dc6821ba24a9ae66dece0e2294922f30ccaa7
Amir Abedi <abedimhosein@gmail.com>
2026-07-08 18:13:36 +0330

    fixing bugs

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,25 @@ if DEBUG:
 
 INTERNAL_IPS = ["127.0.0.1"]
 
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG,
+}
+
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.history.HistoryPanel",
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+]
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
